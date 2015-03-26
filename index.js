70deb1a0369741c17c69df6bd7434a005ecad61b
omaksi <ondrej.maksi@gmail.com>
2015-03-26 10:50:44 +0100

    added logging to access.log

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var finalhandler = require('finalhandler')
 var http = require('http')
 
+var fs = require('fs')
+var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
+
 var serveStatic = require('serve-static')
 var serve = serveStatic('public')
 
 var morgan = require('morgan')
-var logger = morgan('dev')
-
+var logger = morgan('dev', {stream: accessLogStream})
 
 var server = http.createServer(function (req, res) {
   var done = finalhandler(req, res)
